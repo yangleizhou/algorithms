79ef6b9e6546cca9d122d3fc4d3fb34c9fbc8f74
leizhouyang <yangleizhou@weile.com>
2020-08-06 15:26:42 +0800

    数字区间按位与

--- binary/201-range_bitwiseand/range_bitwiseand.go
+++ binary/201-range_bitwiseand/range_bitwiseand.go
@@ -1,5 +1,7 @@
 package binary
 
+import "fmt"
+
 // 解法一
 func rangeBitwiseAnd1(m int, n int) int {
 	if m == 0 {
@@ -7,17 +9,24 @@ func rangeBitwiseAnd1(m int, n int) int {
 	}
 	moved := 0
 	for m != n {
+		if n == 0 {
+			return 0
+		}
 		m >>= 1
 		n >>= 1
 		moved++
 	}
+	fmt.Println(moved)
 	return m << uint32(moved)
 }
 
 // 解法二 Brian Kernighan's algorithm
-func rangeBitwiseAnd(m int, n int) int {
+func rangeBitwiseAnd2(m int, n int) int {
+	var count int
 	for n > m {
+		count++
 		n &= (n - 1) // 清除最低位的 1
 	}
+	fmt.Println(count)
 	return n
 }
--- binary/201-range_bitwiseand/range_bitwiseand_test.go
+++ binary/201-range_bitwiseand/range_bitwiseand_test.go
@@ -11,7 +11,15 @@ func TestRangeBitwiseAnd(t *testing.T) {
 	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd1(5, 7), rangeBitwiseAnd1(5, 7))
 	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd1(0, 1), rangeBitwiseAnd1(0, 1))
 	fmt.Println()
-	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd(20, 25), rangeBitwiseAnd(20, 25))
-	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd(5, 7), rangeBitwiseAnd(5, 7))
-	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd(0, 1), rangeBitwiseAnd(0, 1))
+	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd2(20, 25), rangeBitwiseAnd2(20, 25))
+	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd2(5, 7), rangeBitwiseAnd2(5, 7))
+	fmt.Printf("0b%b = %d\n", rangeBitwiseAnd2(0, 1), rangeBitwiseAnd2(0, 1))
+}
+
+func TestRangeBitwiseAndCmp1(t *testing.T) {
+	rangeBitwiseAnd1(1000, 262143)
+}
+
+func TestRangeBitwiseAndCmp2(t *testing.T) {
+	rangeBitwiseAnd2(1000, 262143)
 }
