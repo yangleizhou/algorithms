cae2400a56d1545bfac40ab73a250161756f98b6
leizhouyang <yangleizhou@weile.com>
2020-08-08 19:38:51 +0800

    直接插入排序算法

--- algorithms/sort/straight_insertion/straight_insertion_test.go
+++ algorithms/sort/straight_insertion/straight_insertion_test.go
@@ -9,5 +9,4 @@ func TestStraightInsertion(t *testing.T) {
 	var nums = []int{20, 3, 5, 2, 89, 6, 2, 4, 3, 10, 11, 7, 80}
 	fmt.Println("input:\n", nums)
 	fmt.Println("value:\n", straightInsertion(nums))
-
 }
