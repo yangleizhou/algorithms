a9f19aecda3936f5a330947a1ceea240a5f5a006
yangleizhou <135624539@qq.com>
2020-09-21 08:13:56 +0800

    冒泡排序

--- algorithms/sort/exchange/bubble/bubble.go
+++ algorithms/sort/exchange/bubble/bubble.go
@@ -1,1 +1,22 @@
-package  exchange
+package exchange
+
+func bubble(nums []int) []int {
+	size := len(nums)
+	for i := 0; i < size-1; i++ {
+		for j := i + 1; j < size-1; j++ {
+			if nums[i] < nums[j] {
+				nums[i], nums[j] = nums[j], nums[i]
+			}
+		}
+	}
+	return nums
+}
+
+func bubbleSort(nums []int, size int) {
+	for i := 0; i < size-1; i++ {
+		if nums[i] > nums[i+1] {
+			nums[i], nums[i+1] = nums[i+1], nums[i]
+		}
+	}
+	bubbleSort(nums, size-1)
+}
